9e88a277cb64e13137ccdf4c6cff096ee860a27f
openkara <karaburney@gmail.com>
2026-04-17 18:30:21 -0500

    Add /survey route page

---
+++ app/survey/page.tsx
@@ -0,0 +1,19 @@
+export default function SurveyPage() {
+  return (
+    <iframe
+      src="/survey.html"
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+        margin: 0,
+        padding: 0,
+        overflow: "hidden",
+      }}
+      title="before we... The Big Money Survey"
+    />
+  );
+}
